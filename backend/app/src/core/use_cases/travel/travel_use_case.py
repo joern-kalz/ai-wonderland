4647df3db5306f52cfb686d76a3e8b0afb599c35
joern-kalz <joern.kalz@gmail.com>
2026-03-26 17:14:56 +0100

    Add image generation to travel use case

--- backend/app/src/core/use_cases/travel/travel_use_case.py
+++ backend/app/src/core/use_cases/travel/travel_use_case.py
@@ -1,7 +1,9 @@
 """Use case for traveling to an NPC."""
 
 from src.adapters.storage.session_store import read_session, write_session
+from src.core.shared.image_generator import generate_npc_image
 from src.core.use_cases.travel.npc_evaluator import map_npc
+from src.model.npc import Npc
 from src.model.travel_result import TravelResult
 
 
@@ -14,12 +16,17 @@ def travel(session_token: str, npc: str) -> TravelResult:
         raise ValueError("Invalid session token")
 
     mapping = map_npc(session, npc)
+    session.log.append(mapping.log)
 
     if mapping.npc is None:
+        write_session(session_token, session)
         return TravelResult(result="not_a_valid_character")
+    else:
+        npc_image = generate_npc_image(mapping.npc)
 
-    session.current_npc = mapping.npc
-    session.log.append(mapping.log)
-    write_session(session_token, session)
+        session.log.append(npc_image.log)
+        session.npcs_by_name[mapping.npc] = Npc(image=npc_image.image, chat_history=[])
+        session.current_npc = mapping.npc
 
-    return TravelResult(result="success")
+        write_session(session_token, session)
+        return TravelResult(result="success")
