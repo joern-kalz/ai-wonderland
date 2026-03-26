953a6445cf74d0639d6969e362bcf3f47af11500
joern-kalz <joern.kalz@gmail.com>
2026-03-26 18:43:43 +0100

    Loading spinner for game start

--- frontend/app/app/_components/Game.tsx
+++ frontend/app/app/_components/Game.tsx
@@ -7,16 +7,19 @@ import { sendStartGame } from "@/libs/sendStartGame";
 
 export default function Game() {
     const [sessionToken, setSessionToken] = useState<string>();
+    const [isLoading, setIsLoading] = useState(false);
 
     async function onStart() {
+        setIsLoading(true);
         const response = await sendStartGame();
         setSessionToken(response.session_token);
+        setIsLoading(false);
     }
 
     if (sessionToken) {
         return <Main sessionToken={sessionToken} />;
     } else {
-        return <Menu onStart={onStart} />;
+        return <Menu onStart={onStart} isLoading={isLoading} />;
     }
 
 }
--- frontend/app/app/_components/menu/Menu.tsx
+++ frontend/app/app/_components/menu/Menu.tsx
@@ -1,14 +1,20 @@
 export interface MenuProps {
     onStart: () => void;
+    isLoading: boolean;
 }
 
-export default function Menu({ onStart }: MenuProps) {
+export default function Menu({ onStart, isLoading }: MenuProps) {
     return (
         <div>
             <button
-                className="p-4 bg-blue-200 hover:bg-blue-300 active:bg-blue-300-300 text-blue-900 active:text-blue-800 border border-gray-400 active:border-gray-500 rounded-md w-50 m-3 cursor-pointer"
-                onClick={onStart}>
-                Enter Wonderland
+                className="p-4 bg-blue-200 hover:bg-blue-300 active:bg-blue-300-300 text-blue-900 active:text-blue-800 border border-gray-400 active:border-gray-500 rounded-md w-50 m-3 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                onClick={onStart}
+                disabled={isLoading}>
+                {isLoading ? (
+                    <div className="animate-spin h-4 w-4 border border-current border-t-transparent rounded-full mx-auto"></div>
+                ) : (
+                    'Enter Wonderland'
+                )}
             </button>
         </div>
     )
