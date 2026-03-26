f887292541262cdaafe55ec88524d46c076efa0e
joern-kalz <joern.kalz@gmail.com>
2026-03-26 18:52:45 +0100

    Get session token from query

--- frontend/app/app/_components/Game.tsx
+++ frontend/app/app/_components/Game.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useState } from "react";
+import { useSearchParams } from 'next/navigation';
 import Menu from "./menu/Menu";
 import Main from "./main/Main";
 import { sendStartGame } from "@/libs/sendStartGame";
 
 export default function Game() {
-    const [sessionToken, setSessionToken] = useState<string>();
+    const searchParams = useSearchParams();
+    const [sessionToken, setSessionToken] = useState<string | undefined>(searchParams.get('session_token') || undefined);
     const [isLoading, setIsLoading] = useState(false);
 
     async function onStart() {
