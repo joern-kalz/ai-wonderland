fafa540001b6c58d1c6dd10723f67d240387dc25
joern-kalz <joern.kalz@gmail.com>
2026-03-26 17:14:22 +0100

    Add log for image generation

--- backend/app/src/core/shared/image_generator.py
+++ backend/app/src/core/shared/image_generator.py
@@ -2,11 +2,14 @@
 
 from src.adapters.ai.image_model import generate_png_image
 from src.core.shared.image_description_generator import generate_image_description
+from src.model.image_generation_result import ImageGenerationResult
 
 
-def generate_npc_image(npc: str) -> bytes:
+def generate_npc_image(npc: str) -> ImageGenerationResult:
     """Generates an image of the NPC"""
 
     response = generate_image_description(npc)
     prompt = response[-1].content
-    return generate_png_image(prompt)
+    image = generate_png_image(prompt)
+
+    return ImageGenerationResult(image=image, log=response)
--- backend/app/src/core/use_cases/start/start_use_case.py
+++ backend/app/src/core/use_cases/start/start_use_case.py
@@ -2,8 +2,8 @@
 
 import secrets
 
-from src.adapters.ai.image_model import generate_png_image
 from src.adapters.storage.session_store import write_session
+from src.core.shared.image_generator import generate_npc_image
 from src.core.use_cases.start.overview_generator import generate_overview
 from src.model.game_session import GameSession
 from src.model.npc import Npc
@@ -13,20 +13,21 @@ def start_game_and_return_session_token() -> str:
     """Starts a new game session."""
 
     overview = generate_overview()
-    npc = overview.current_npc
+    npc_image = generate_npc_image(overview.current_npc)
 
     session = GameSession(
         crisis=overview.crisis,
         quests=overview.quests,
+        current_quest=0,
         actions_since_quest_start=0,
         current_npc=overview.current_npc,
         npcs_by_name={
             overview.current_npc: Npc(
-                image=generate_png_image(overview.current_npc),
+                image=npc_image.image,
                 chat_history=[],
             )
         },
-        log=[overview.log],
+        log=[overview.log, npc_image.log],
     )
 
     sessionToken = secrets.token_urlsafe(32)
