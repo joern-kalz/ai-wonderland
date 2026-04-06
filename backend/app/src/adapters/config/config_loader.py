a95e21b0b82a1993f5728724614a627b1882b165
joern-kalz <joern.kalz@gmail.com>
2026-04-06 17:29:16 +0200

    Load API keys from Secret Manager when on AWS

--- backend/app/src/adapters/config/config_loader.py
+++ backend/app/src/adapters/config/config_loader.py
@@ -1,7 +1,23 @@
 """Loader for the app configuration"""
 
+import os
+
+import boto3
 from dotenv import load_dotenv
+
+from src.adapters.config.global_config_provider import is_on_aws
 
 
 def load_config() -> None:
+    """Loads the app configuration"""
     load_dotenv()
+
+    if is_on_aws():
+        load_key("OPENAI")
+        load_key("GROQ")
+
+
+def load_key(name: str) -> None:
+    client = boto3.client("secretsmanager")
+    response = client.get_secret_value(SecretId=os.environ[f"{name}_SECRET_NAME"])
+    os.environ[f"{name}_API_KEY"] = response["SecretString"]
