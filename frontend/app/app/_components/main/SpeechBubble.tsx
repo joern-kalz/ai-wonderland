b86ff8d684eb2ce81bd14f7213dc0aa2548bf918
joern-kalz <joern.kalz@gmail.com>
2026-04-08 19:43:11 +0200

    Improve UI

--- frontend/app/app/_components/main/SpeechBubble.tsx
+++ frontend/app/app/_components/main/SpeechBubble.tsx
@@ -8,7 +8,7 @@ export default function SpeechBubble({ message }: SpeechBubbleProps) {
     }
 
     return (
-        <div className="absolute top-1/8 left-1/2 -translate-x-1/2 p-4 bg-white opacity-90 text-gray-950 rounded-lg border-2 border-gray-800 border-solid w-1/2 text-center">
+        <div className="absolute top-1/32 left-1/2 -translate-x-1/2 p-4 bg-white opacity-90 text-gray-950 rounded-lg border-2 border-gray-800 border-solid w-1/2 text-center">
             {message}
             <div className="absolute left-1/2 -translate-x-1/2 bottom-[-1px] translate-y-full w-0 h-0 border-l-[20px] border-r-[20px] border-t-[26px] border-l-transparent border-r-transparent border-t-gray-800" />
             <div className="absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-full w-0 h-0 border-l-[18px] border-r-[18px] border-t-[24px] border-l-transparent border-r-transparent border-t-white" />
